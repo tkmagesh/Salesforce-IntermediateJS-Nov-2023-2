9ba546f68bda58789eba34425f7ffac937f17d12
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-21 17:20:14 +0530

    isPrime - memoization - exercise - solution

--- 04-closures/memoization.js
+++ 04-closures/memoization.js
@@ -1,8 +1,18 @@
-function isPrime(no){
-    console.log('processing : ', no)
-    for (var i = 2; i <= (no/2); i++){
-        if (no % 2 === 0)
-            return false;
+var isPrime = (function(){
+    var cache = {}
+    function isPrime(no){
+        if (cache.hasOwnProperty(no)){
+            return cache[no]
+        }
+        console.log('processing : ', no)   
+        for (var i = 2; i <= (no/2); i++){
+            if (no % 2 === 0){
+                cache[no] = false;
+                return cache[no];
+            }
+        }
+        cache[no] = true;
+        return cache[no];
     }
-    return true;
-}
+    return isPrime;
+})()
