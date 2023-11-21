b88ede3df483f5a06b1596c3df3bf8c7045158ff
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-21 16:00:24 +0530

    spinner - assignment

---
+++ 04-closures/spinner.js
@@ -0,0 +1,33 @@
+/* 
+Create an object and assign it to a variable 'spinner'
+Spinner Methods:
+    - up()
+    - down()
+
+Behaviors
+    spinner.up() //=> 1
+    spinner.up() //=> 2
+    spinner.up() //=> 3
+    spinner.up() //=> 4
+
+    spinner.down() //=> 3
+    spinner.down() //=> 2
+    spinner.down() //=> 1
+    spinner.down() //=> 0
+    spinner.down() //=> -1
+
+IMPORTANT NOTE:
+    NO HTML PAge, No button, No click event
+    
+    there SHOULD NOT be any way by which the outcome of up() and down() methods can be influenced from outside
+
+    the following SHOULD NOT be possible
+    window.count = 1000
+    spinner.up() //=> 1001
+
+    OR
+    spinner.count = 1000
+    spinner.up() //=> 1001
+*/
+
+var spinner = /* ..... */
