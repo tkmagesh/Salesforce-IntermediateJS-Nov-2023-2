8b99c68f3d3e1875853b352a5c918c726884c20e
Magesh Kuppan <tkmagesh77@gmail.com>
2023-11-21 17:28:45 +0530

    Creating an utility function for memoization

--- 04-closures/memoization.js
+++ 04-closures/memoization.js
@@ -16,3 +16,45 @@ var isPrime = (function(){
     }
     return isPrime;
 })()
+
+var isOddOrEven = (function(){
+    var cache = {}
+    function isOddOrEven(no){
+        if (cache.hasOwnProperty(no)){
+            return cache[no]
+        }
+        console.log('processing : ', no)   
+        cache[no] = nos % 2 === 0 ? 'even' : 'odd';
+        return cache[no];
+    }
+    return isOddOrEven;
+})()
+
+
+// Creating an utility function for memoization
+
+function memoize(fn){
+    var cache = {}
+    return function (no){
+        if (cache.hasOwnProperty(no)){
+            return cache[no]
+        }
+        console.log('processing : ', no)   
+        cache[no] = fn(no)
+        return cache[no];
+    }
+}
+
+var isOddOrEven = memoize(function(no){
+    return no % 2 === 0 ? 'even' : 'odd'
+})
+
+var isPrime = memoize(function(no){
+    for (var i = 2; i <= (no/2); i++){
+        if (no % 2 === 0){
+            return false
+        }
+    }
+    return true
+})
+
